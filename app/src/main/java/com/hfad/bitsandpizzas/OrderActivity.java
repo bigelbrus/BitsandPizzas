acc7d5f8ae7e683e218b8a764a5681d626d4c275
Alex Levchenko <bigelbrus@gmail.com>
2019-11-18 19:52:19 +0500

    Add FAB to activity_order and Image to CollapsingToolbarLayout

--- app/src/main/java/com/hfad/bitsandpizzas/OrderActivity.java
+++ app/src/main/java/com/hfad/bitsandpizzas/OrderActivity.java
@@ -1,9 +1,13 @@
 package com.hfad.bitsandpizzas;
 
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
 
 public class OrderActivity extends AppCompatActivity {
 
@@ -15,5 +19,19 @@ public class OrderActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    public void onClickDone(View view) {
+        View contextView = findViewById(R.id.coordinator);
+        CharSequence charSequence = "Tap UNDO to make Toast";
+        Snackbar.make(contextView,charSequence,Snackbar.LENGTH_SHORT)
+                .setAction("UNDO", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(OrderActivity.this, "You make UNDO!", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .show();
+
     }
 }
